9228b26d13c9c0bef4743065774e9a3d34eb4f85
rohithpalagiri <rohithpalagiri@gmail.com>
2020-06-19 14:44:36 -0400

    adding build

---
+++ build/precache-manifest.abc176350ac13f13f192f6d527f9b02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe2e73cae3deff542d8b568bdb33eae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15290268a0431a279cb0",
+    "url": "/static/css/main.18d6726e.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15290268a0431a279cb0",
+    "url": "/static/js/main.85785a4b.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 
 let persons = [
     {
